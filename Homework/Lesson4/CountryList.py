9b8ec11544cba969d9cdf1f2ba7fc590899b46ce
FirstTi <JustInSluchay@protonmail.com>
2022-05-19 14:47:54 +0300

    Home

---
+++ Homework/Lesson4/CountryList.py
@@ -0,0 +1,18 @@
+# Нужно данный список сохранить в файл, а затем прочитать из файла и вывести в консоль (использовать модуль json)
+
+import json
+
+countries = ["Canada", "Bulgaria", "Germany"]
+
+# f = open("CountryList.json", "w")
+# json.dump(countries, f)
+# f.close()
+
+
+with open("CountryList.json", "w") as f:
+    json.dump(countries, f)
+
+with open ("CountryList.json", "r") as f:
+    countries = json.load(f)
+    print(countries)
+
